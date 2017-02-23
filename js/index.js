0cbcf8605a6f2a7b794e9fbfaddad62353b84e8b
Borja Fernandez (uniLeon.Inf.TFG2016-17.MagoBestias) <bfernm04@estudiantes.unileon.es>
2017-02-23 17:20:07 +0100

    update

--- js/index.js
+++ js/index.js
@@ -1,7 +1,43 @@
+var rest = "https://coleccioncoches.azurewebsites.net/";
 
+function paint(data){
+
+}
 
 function addElement(){
-	alert("Hola mundo");
+	data = {
+        marca : $("#marca").val(),
+        modelo : $('#modelo').val(),
+        year : $('#year').val(),
+        nacionalidad_coche : $('#nacionalidad_coche').val(),
+        enlace_foto : $('#foto').val(),
+        tipo : $('#tipo option:selected').val(),
+        campeonato : $('#campeonato').val(),
+        competicion : $('#competicion').val(),
+        categoria : $('#categoria').val(),
+        piloto : $('#piloto').val(),
+        nacionalidad_piloto : $('#nacionalidad_piloto').val(),
+        copiloto : $('#copiloto').val(),
+        nacionalidad_copiloto : $('#nacionalidad_copiloto').val(),
+        fabricante : $('#fabricante').val(),
+        precio : $('#precio').val(),
+        descripcion : $('#descripcion').val()
+    };
+
+      $.ajax({
+          "crossDomain": true,
+          "url": rest+"/add_element",
+          "method": "POST",
+          "headers": { "content-type": "application/json" },
+          //"data": "{\n \"type\":\""+data.AnalyticCODE+"\",\n  \"value\":\""+data.AnalyticVALUE+"\"\n}"
+          "data": data
+      }).done(function (response) {
+
+          //data = {AnalyticCODE : response.AnalyticCODE, AnalyticVALUE : response.AnalyticVALUE, Analytic_ID: response.Analytic_ID};
+
+          //paint(data);
+
+      });
 }
 
 function searchElements(){
