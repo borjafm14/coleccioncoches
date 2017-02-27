b6c2bfe15d146669e7769e4d2787db9d1499e8a7
Borja Fernandez (uniLeon.Inf.TFG2016-17.MagoBestias) <bfernm04@estudiantes.unileon.es>
2017-02-27 19:02:06 +0100

    update

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.get('/list_last_elements', function (req, res) {
 
      if(!err){
        //console.log(recordsets[0]);
-       res.status(200).send(JSON.stringify(recordsets));
+       res.status(200).send(JSON.stringify(recordsets[0]));
      }else{
         console.log(err);
        res.status(400).send(err);
@@ -65,92 +65,21 @@ app.post('/add_element', function (req, res) {
       request.input('marca', sql.NVarChar, req.body.marca.toUpperCase());
       request.input('modelo', sql.NVarChar, req.body.modelo.toUpperCase());
 
-      if(req.body.year){
-        request.input('year', sql.NVarChar, req.body.year.toUpperCase());
-      }
-      else{
-        request.input('year', sql.NVarChar, "");
-      }
-      
-      if(req.body.nacionalidad_coche){
-        request.input('nacionalidad_coche', sql.NVarChar, req.body.nacionalidad_coche.toUpperCase());
-      }
-      else{
-        request.input('nacionalidad_coche', sql.NVarChar, "");
-      }
+      request.input('year', sql.NVarChar, req.body.year.toUpperCase());
+      request.input('nacionalidad_coche', sql.NVarChar, req.body.nacionalidad_coche.toUpperCase());
       
       request.input('tipo', sql.Int, req.body.tipo);
       request.input('enlace_foto', sql.NVarChar, req.body.enlace_foto);
-
-      if(req.body.campeonato){
-        request.input('campeonato', sql.NVarChar, req.body.campeonato.toUpperCase());
-      }
-      else{
-        request.input('campeonato', sql.NVarChar, "");
-      }
-
-      if(req.body.competicion){
-        request.input('competicion', sql.NVarChar, req.body.competicion.toUpperCase());
-      }
-      else{
-        request.input('competicion', sql.NVarChar, "");
-      }
-
-      if(req.body.categoria){
-        request.input('categoria', sql.NVarChar, req.body.categoria.toUpperCase());
-      }
-      else{
-        request.input('categoria', sql.NVarChar, "");
-      }
-      
-      if(req.body.piloto){
-        request.input('piloto', sql.NVarChar, req.body.piloto.toUpperCase());
-      }
-      else{
-        request.input('piloto', sql.NVarChar, "");
-      }
-
-      if(req.body.nacionalidad_piloto){
-        request.input('nacionalidad_piloto', sql.NVarChar, req.body.nacionalidad_piloto.toUpperCase());
-      }
-      else{
-        request.input('nacionalidad_piloto', sql.NVarChar, "");
-      }
-
-      if(req.body.copiloto){
-        request.input('copiloto', sql.NVarChar, req.body.copiloto.toUpperCase());
-      }
-      else{
-        request.input('copiloto', sql.NVarChar, "");
-      }
-
-      if(req.body.nacionalidad_copiloto){
-        request.input('nacionalidad_copiloto', sql.NVarChar, req.body.nacionalidad_copiloto.toUpperCase());
-      }
-      else{
-        request.input('nacionalidad_copiloto', sql.NVarChar, "");
-      }
-
-      if(req.body.precio){
-        request.input('precio', sql.NVarChar, req.body.precio.toUpperCase());
-      }
-      else{
-        request.input('precio', sql.NVarChar, "");
-      }
-      
-      if(req.body.descripcion){
-        request.input('descripcion', sql.NVarChar, req.body.descripcion);
-      }
-      else{
-        request.input('descripcion', sql.NVarChar, "");
-      }
-
-      if(req.body.fabricante){
-        request.input('fabricante', sql.NVarChar, req.body.fabricante.toUpperCase());
-      }
-      else{
-        request.input('fabricante', sql.NVarChar, "");
-      }
+      request.input('campeonato', sql.NVarChar, req.body.campeonato.toUpperCase());
+      request.input('competicion', sql.NVarChar, req.body.competicion.toUpperCase());
+      request.input('categoria', sql.NVarChar, req.body.categoria.toUpperCase());
+      request.input('piloto', sql.NVarChar, req.body.piloto.toUpperCase());
+      request.input('nacionalidad_piloto', sql.NVarChar, req.body.nacionalidad_piloto.toUpperCase());
+      request.input('copiloto', sql.NVarChar, req.body.copiloto.toUpperCase());
+      request.input('nacionalidad_copiloto', sql.NVarChar, req.body.nacionalidad_copiloto.toUpperCase());
+      request.input('precio', sql.NVarChar, req.body.precio.toUpperCase());
+      request.input('descripcion', sql.NVarChar, req.body.descripcion);
+      request.input('fabricante', sql.NVarChar, req.body.fabricante.toUpperCase());
 
       request.execute('CARS__insert', function(err, recordsets, returnValue, affected) {
           if(!err){
